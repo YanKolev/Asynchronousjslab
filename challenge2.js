bd2cb1725a818418580a644ee2dfbad107588dfa
Yan Kolev <y.kolev11@gmail.com>
2024-08-21 15:05:23 +0200

    Challenge2 Solution

--- challenge2.js
+++ challenge2.js
@@ -31,3 +31,108 @@ image path. Set the network speed to “Fast 3G” in the dev tools Network tab,
 otherwise images load too fast
 
 */
+
+/*
+const wait = function(seconds){
+    return new Promise(function(reslove){
+      setTimeout(reslove,seconds * 1000)
+    })
+  };
+
+
+const imgContainer = document.querySelector('.images')
+
+const createImage = function (imgPath){
+    return new Promise(function(resolve, reject){
+        const img = document.createElement ('img');
+        img.src = imgPath;
+
+        img.addEventListener('load', function(){
+            imgContainer.append(img);
+            resolve(img)
+
+        });
+
+        img.addEventListener('error',function(){
+            reject(new Error('Image not found'))
+        })
+    })
+}
+
+let currentImg;
+
+
+createImage('img/img-1.jpg')
+    .then(img => {
+        currentImg = img;
+        console.log('Image 1 loaded');
+        wait(2)
+
+})
+    .then(() => {
+        currentImg.style.display = 'none';
+        return createImage('img/img-2.jpg')
+
+    })
+    .then(img => {
+        currentImg = img;
+        console.log('Image 2 loaded')
+        return wait(2)
+    })
+    .then(() => {
+        currentImg.style.display = 'none';
+    })
+    .catch(err => console.error(err))
+
+
+
+*/
+
+
+// NEW Variant 
+
+const wait = function(seconds) {
+    return new Promise(function(resolve) {
+        setTimeout(resolve, seconds * 1000);
+    });
+};
+
+const imgContainer = document.querySelector('.images');
+
+const createImage = function(imgPath) {
+    return new Promise(function(resolve, reject) {
+        const img = document.createElement('img');
+        img.src = imgPath;
+
+        img.addEventListener('load', function() {
+            imgContainer.append(img);
+            resolve(img);
+        });
+
+        img.addEventListener('error', function() {
+            reject(new Error('Image not found'));
+        });
+    });
+};
+
+let currentImg;
+
+createImage('img/img-1.jpg')
+    .then(img => {
+        currentImg = img;
+        console.log('Image 1 loaded');
+        return wait(2);  // Return the wait(2) promise to chain the timing correctly
+    })
+    .then(() => {
+        currentImg.style.display = 'none';
+        return createImage('img/img-2.jpg');
+    })
+    .then(img => {
+        currentImg = img;
+        console.log('Image 2 loaded');
+        return wait(2);
+    })
+    .then(() => {
+        currentImg.style.display = 'none';
+    })
+    .catch(err => console.error(err));

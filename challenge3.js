3a450ac2c3a13d0f1171c1ff3724f9747a9e4c3c
Yan Kolev <y.kolev11@gmail.com>
2024-08-24 21:27:11 +0200

    solution challenge3

--- challenge3.js
+++ challenge3.js
@@ -21,3 +21,93 @@ Test data Part 2: ['img/img-1.jpg', 'img/img-2.jpg', 'img/img-
 3.jpg']. To test, turn off the 'loadNPause' function
 GOOD LUCK 😀
 */
+
+const wait = function(seconds) {
+    return new Promise(function(resolve) {
+        setTimeout(resolve, seconds * 1000);
+    });
+};
+
+const imgContainer = document.querySelector('.images');
+
+const createImage = function(imgPath) {
+    return new Promise(function(resolve, reject) {
+        const img = document.createElement('img');
+        img.src = imgPath;
+
+        img.addEventListener('load', function() {
+            imgContainer.append(img);
+            resolve(img);
+        });
+
+        img.addEventListener('error', function() {
+            reject(new Error('Image not found'));
+        });
+    });
+};
+
+let currentImg;
+
+/*
+createImage('img/img-1.jpg')
+    .then(img => {
+        currentImg = img;
+        console.log('Image 1 loaded');
+        return wait(2);  // Return the wait(2) promise to chain the timing correctly
+    })
+    .then(() => {
+        currentImg.style.display = 'none';
+        return createImage('img/img-2.jpg');
+    })
+    .then(img => {
+        currentImg = img;
+        console.log('Image 2 loaded');
+        return wait(2);
+    })
+    .then(() => {
+        currentImg.style.display = 'none';
+    })
+    .catch(err => console.error(err));
+
+*/
+
+const loadNPause = async function () {
+    try{
+        //Load img 1 
+     let img1 = await createImage('img/img-1.jpg')
+     console.log('Image 1 loaded');
+     await wait(2);//pausing execution for 2 seconds.
+     img.stle.display = 'none'
+
+     //Load img 2 
+     img = await createImage('img/img-2.jpg')
+     console.log('Image 2 loaded');
+     await wait(2);//pausing execution for 2 seconds.
+     img.stle.display = 'none'
+
+    }catch(err){
+        console.error(err)
+
+    }
+}
+
+const loadAll = async function(imgArr) {
+    try{
+        const imgs = imgArr.map(async img =>  await createImage(img));
+        console.log(imgs)
+
+        const imgsEL = await Promise.all(imgs); //async await with a map u can handle it with promise.all
+        console.log(imgsEL);
+        imgsEL.forEach(img => img.classList.add('parallel'))
+
+    }catch(err){
+        console.error(err);
+    }
+}
+
+loadAll(['img/img-1.jpg', 'img/img-2.jpg', 'img/img-3.jpg']);
+//what we have here is array of promises, not the images themsleves 
+//line 96- > async function and then w ehave arrow function which is explicit return in each adoration 
+//async will  always return a promise and not really yhe value that we are interested in
+//the value that we are looking for is going to be the fullfilled value of the that we are looking for
+//
